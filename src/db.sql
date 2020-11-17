069d43126c9fa866f5adeb12063fabcceb19593f
Kirill Bondyk <54686869+kirilinsane@users.noreply.github.com>
2020-11-17 18:46:17 +0200

    add db.sql

--- src/db.sql
+++ src/db.sql
@@ -1,1 +1,24 @@
 
+CREATE TABLE Product (
+  name varchar(50) NOT NULL,
+  description varchar(50) NOT NULL,
+  price integer NOT NULL,
+  username varchar(50) NOT NULL);
+  ;
+
+CREATE TABLE User (
+  user_name varchar(50) NOT NULL,
+  login varchar(50) primary key, 
+  password varchar(50) NOT NULL );
+
+CREATE TABLE Rating (
+  id integer PRIMARY KEY,
+  value integer NOT NULL,
+  user_name varchar(50) NOT NULL,
+  product_name varchar(50) );
+
+CREATE TABLE Comment (
+  id integer PRIMARY KEY,
+  content text,
+user_name varchar(50) NOT NULL,
+product_name varchar(50) );
